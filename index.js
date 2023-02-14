71962619672d10acc59a3f30ec866c4ee9f8936d
j3553pat <jessecodedev@gmail.com>
2023-02-13 20:34:30 -0600

    worked on

---
+++ index.js
@@ -0,0 +1,50 @@
+// "use strict";
+
+const assert = require("assert");
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
+
+console.log("hello world")
+let finalWord = [["hello"],
+["robot"],
+["jazz"],
+["whisper"],
+["price"],
+["past"]]
+
+let selectWord = ""
+let chancesLeft = 6
+let guessWord = [];
+
+let item = finalWord[Math.floor(Math.random()*finalWord.length) + 1];
+
+console.log(item)
+
+const userInput = (letter) => {
+ let ifRight = false
+ for(let i = 0; i < selectWord.length; i++) {
+    ifRight= true
+    guessWord[i] = letter
+ }
+ return ifRight
+};
+
+const checkForWIn = () => {
+    return guessWord.join('') === selectWord
+}
+
+const execute = () => {
+    console.log('Lets Play!')
+    selectWord()
+    console.log('The word is: ' + guessWord.join(''))
+}
+
+let turn =function () {
+    console.log( )
+}
+
