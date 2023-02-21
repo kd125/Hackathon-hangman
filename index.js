5be8fd4a2494baf3489605e61b32aeef275cc94e
Kd125 <kashdangar1250@gmail.com>
2023-02-20 20:08:24 -0600

    finished

--- index.js
+++ index.js
@@ -2,53 +2,62 @@
 
 const assert = require("assert");
 const readline = require("readline");
+const { start } = require("repl");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
+let finalWord = ["hello", "robot", "jazz", "whisper", "price", "past"];
 
-
-console.log("hello world")
-let finalWord = [["hello"],
-["robot"],
-["jazz"],
-["whisper"],
-["price"],
-["past"]]
-
-let selectWord = ""
-let chancesLeft = 6
+let chancesLeft = 6;
 let guessWord = [];
 
-let item = finalWord[Math.floor(Math.random()*finalWord.length) + 1];
+let selectWord = finalWord[Math.floor(Math.random() * finalWord.length)];
 
-console.log(item)
+const startGame = () => {
+  for (let i = 0; i < selectWord.length; i++) {
+    guessWord.push("_");
+  }
+};
 
-const userInput = (letter) => {
- let ifRight = false
- for(let i = 0; i < selectWord.length; i++) {
-    if(selectWord[i] === letter) {
-        ifRight= true
-        guessWord[i] = letter 
+const checkLetter = (letter) => {
+  for (let i = 0; i < selectWord.length; i++) {
+    if (letter === selectWord[i]) {
+      guessWord[i] = letter;
     }
- }
- return ifRight
+  }
+  return selectWord.includes(letter);
 };
 
 const checkForWIn = () => {
-    return guessWord.join('') === selectWord
-}
+  return guessWord.join("") === selectWord;
+};
 
-let turn = function () {
-    console.log()
-}
+let turn = () => {
+  console.log("You have " + chancesLeft + " tries remaining.");
+  rl.question("Choose a letter: ", (letter) => {
+    if (!checkLetter(letter)) {
+      chancesLeft--;
+    }
+    console.log("Word Progress: " + guessWord.join(""));
+    if (checkForWIn()) {
+      console.log("Congrats! You've won! The word is: " + selectWord);
+      rl.close();
+    } else if (chancesLeft === 0) {
+      console.log("You lose. The word was: " + selectWord);
+      rl.close();
+    } else {
+      turn();
+    }
+  });
+};
 
 const execute = () => {
-    console.log('Lets Play!')
-    selectWord()
-    console.log('The word is: ' + guessWord.join(''))
-}
+  console.log("Lets Play!");
+  startGame();
+  console.log("The word is: " + guessWord.join(""));
+  turn();
+};
 
-
-
+execute();
